b8970b61824926781833d072b362566513e653c3
thinkAmi <dev.thinkami+git@gmail.com>
2024-06-16 13:19:13 +0900

    fix to named entrypoint

--- packages/app-page/src/index.tsx
+++ packages/app-page/src/index.tsx
@@ -9,7 +9,6 @@ app.get('/', async (c) => {
   // awaitを使う理由はこちらに記載あり
   // https://developers.cloudflare.com/workers/runtime-apis/rpc#all-calls-are-asynchronous
   const r = await c.env.MY_WORKER.hello()
-  // const r = await c.env.MY_WORKER_BINDING.hello()
 
   return c.render(r)
 })
